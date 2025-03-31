9624aa32fe6b72231f25cbf3fee234e694e17a48
AnishBhat23 <emsonanish12@gmail.com>
2025-03-31 16:48:22 +0530

    Pong update 2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import time
 from turtle import *
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.setup(height=600,width=800)
 screen.bgcolor("black")
@@ -7,17 +11,42 @@ screen.title("Pong")
 screen.tracer(0)
 screen.listen()
 
-paddle1 = Paddle(-380)
-screen.onkey(fun=paddle1.go_up, key="w")
-screen.onkey(fun=paddle1.go_down, key="s")
+l_paddle = Paddle(-380)
+screen.onkeypress(fun=l_paddle.go_up, key="w")
+screen.onkeypress(fun=l_paddle.go_down, key="s")
 
-paddle2 = Paddle(370)
-screen.onkey(fun=paddle2.go_up, key="Up")
-screen.onkey(fun=paddle2.go_down, key="Down")
+r_paddle = Paddle(370)
+screen.onkeypress(fun=r_paddle.go_up, key="Up")
+screen.onkeypress(fun=r_paddle.go_down, key="Down")
+
+ball = Ball()
+scoreboard = Scoreboard()
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
+    time.sleep(ball.move_speed)
+    ball.move()
+
+    #Detect Collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x_r_paddle()
+
+    # Detect collision with left paddle
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x_l_paddle()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
